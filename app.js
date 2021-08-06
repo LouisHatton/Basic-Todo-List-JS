c63e76a10ec19a346cf6dcc9d94b042b9a444fda
Louis Haton <71732103+LouisHatton@users.noreply.github.com>
2021-08-06 10:57:21 +0100

    Added Filters

--- app.js
+++ app.js
@@ -2,11 +2,13 @@
 const todoInput = document.querySelector('.todo-input');
 const todoButton = document.querySelector('.todo-button');
 const todoList = document.querySelector('.todo-list');
+const filterOption = document.querySelector('.filter-todo')
 
 
 //Event Listeners
 todoButton.addEventListener('click', addTodo);
 todoList.addEventListener('click', listButtonClick);
+filterOption.addEventListener('click', filterTodo);
 
 
 
@@ -65,4 +67,30 @@ function listButtonClick(event){
     if(item.classList[0] == "complete-btn") {
         todo.classList.toggle("completed");
     }
+}
+
+function filterTodo(e) {
+    const todos = todoList.childNodes;
+    todos.forEach(function(todo){
+        switch(e.target.value) {
+            case "all": //if the option All is selected
+                todo.style.display = "flex";
+                break;
+            case "completed":
+                if(todo.classList.contains('completed')) {
+                    todo.style.display = "flex";
+                } else {
+                    todo.style.display = "none";
+                }
+                break;
+            case "uncompleted":
+                if(!todo.classList.contains('completed')) {
+                    todo.style.display = "flex";
+                } else {
+                    todo.style.display = "none";
+                }
+                break;
+        }
+    })
+    console.log(todos)
 }
