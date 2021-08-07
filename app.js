0cd0f4d7004a30ec73db04076163a3bc99eeb0a9
Louis Haton <71732103+LouisHatton@users.noreply.github.com>
2021-08-07 11:43:56 +0100

    Added Local Storage

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ const filterOption = document.querySelector('.filter-todo')
 
 
 //Event Listeners
+document.addEventListener('DOMContentLoaded', getLocalTodos);
+
 todoButton.addEventListener('click', addTodo);
 todoList.addEventListener('click', listButtonClick);
 filterOption.addEventListener('click', filterTodo);
@@ -16,8 +18,6 @@ filterOption.addEventListener('click', filterTodo);
 function addTodo(event){
     event.preventDefault(); //prevent form from submitting
     
-    console.log("button clicked!");
-
     //Todo DIV
     const todoDiv = document.createElement('div');
     todoDiv.classList.add("todo");
@@ -28,6 +28,9 @@ function addTodo(event){
     newTodo.classList.add("todo-item");
 
     todoDiv.appendChild(newTodo); //putting the LI into the DIV
+
+    //Add todo to Local Storage
+    saveLocalTodos(todoInput.value)
 
     //checkmark Button
     const completedButton = document.createElement("button");
@@ -56,6 +59,7 @@ function listButtonClick(event){
     if (item.classList[0] == "trash-btn"){
         //add the animation(transition) class
         todo.classList.add("fall")
+        removeLocalTodos(todo);
         
         //remove the div once transition has finished:
         todo.addEventListener("transitionend", function(){ 
@@ -93,4 +97,76 @@ function filterTodo(e) {
         }
     })
     console.log(todos)
+}
+
+function saveLocalTodos(todo) {
+    //CHECK - are there already items avaliable?
+    let todos;
+    if(localStorage.getItem('todos') === null) {
+        todos = []; //if not set blank array
+    } else {
+        todos = JSON.parse(localStorage.getItem('todos')) //if yes, get them
+    }
+
+    //add new item
+    todos.push(todo); 
+
+    //save back into local storage
+    localStorage.setItem("todos", JSON.stringify(todos)); 
+
+}
+
+function getLocalTodos(){
+    //CHECK - are there already items avaliable?
+    let todos;
+    if(localStorage.getItem('todos') === null) {
+        todos = []; //if not set blank array
+    } else {
+        todos = JSON.parse(localStorage.getItem('todos')) //if yes, get them
+    }
+
+    todos.forEach(function(todo){
+        //Todo DIV
+        const todoDiv = document.createElement('div');
+        todoDiv.classList.add("todo");
+
+        //Create LI
+        const newTodo = document.createElement('li');
+        newTodo.innerText = todo;
+        newTodo.classList.add("todo-item");
+
+        todoDiv.appendChild(newTodo); //putting the LI into the DIV
+
+        //checkmark Button
+        const completedButton = document.createElement("button");
+        completedButton.innerHTML = "<i class='fas fa-check'></i>";
+        completedButton.classList.add("complete-btn");
+        todoDiv.appendChild(completedButton);
+
+        //trash Button
+        const trashButton = document.createElement("button");
+        trashButton.innerHTML = "<i class='fas fa-trash'></i>";
+        trashButton.classList.add("trash-btn");
+        todoDiv.appendChild(trashButton);
+
+        //Append To List **
+        todoList.appendChild(todoDiv);
+    })
+
+}
+
+function removeLocalTodos(todo) {
+    //CHECK - are there already items avaliable?
+    let todos;
+    if(localStorage.getItem('todos') === null) {
+        todos = []; //if not set blank array
+    } else {
+        todos = JSON.parse(localStorage.getItem('todos')) //if yes, get them
+    }
+
+    const todoIndex = todos.indexOf(todo.children[0].innerText);
+    console.log(todoIndex);
+    todos.splice(todoIndex, 1);
+    
+    localStorage.setItem("todos", JSON.stringify(todos)); 
 }
